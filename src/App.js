cf645ce5559b68dc54d6d333906d23d9d509fef1
Monisharaja-s <monishasukumaran07@gmail.com>
2024-03-20 12:29:45 +0530

    css for the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
