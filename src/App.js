6da20c5757c3c15bfc57eb18639c47220de0f372
Monisharaja-s <monishasukumaran07@gmail.com>
2024-03-20 12:07:37 +0530

    product images added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Card from './Card';
 
 function App() {
   return (
